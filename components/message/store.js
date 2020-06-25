e3647d7a5f76443306c26f5ed73da0dc4f6d0225
afcarrion <af.carrion@uniandes.edu.co>
2020-06-24 23:02:43 -0500

    Problema conexion base de datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,24 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+
+/* mongodb+srv://user:user123@cluster0-m7wl6.mongodb.net/telegrame?retryWrites=true&w=majority */
+db.Promise = global.Promise;
+
+db.connect(
+  "mongodb+srv://user:user123@cluster0-m7wl6.mongodb.net/telegrame?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+console.log("Conexion ok");
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
+  console.log(message);
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
